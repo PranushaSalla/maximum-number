8b3753c213d615d620b71a83efd3c29af45b85d8
PranushaSalla <101416724+PranushaSalla@users.noreply.github.com>
2022-03-28 23:00:44 +0530

    Add files via upload
    program to find maximum number from provided input w/o any library function

---
+++ array.ts
@@ -0,0 +1,10 @@
+let array=[1,2,101,45,55,1443];
+ let max=array[0];
+for(let i = 0; i<array.length; i++)
+{
+    if(array[i]>max) { max=array[i];}
+    {
+        max=array[i];
+    }
+}
+console.log("max="+max);
